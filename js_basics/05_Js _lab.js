77329ae3b07ef3ab02167919f869fa69a0aa9da4
monami2997 <immonami2997@gmail.com>
2026-05-08 08:37:29 +0530

    Add 05_Js _lab.js file

---
+++ js_basics/05_Js _lab.js
@@ -0,0 +1,36 @@
+// var name="Monami";
+var firstName="Monami"; //camelcase
+var lastName= "Chatterjee";
+let userName = "camelCase";
+let totalPrice = 99.99;
+let isLoggedIn = true;
+
+let UserProfile = "PascalCase";
+let ShoppingCart = "class name style";//pascalcase
+
+let user_name = "snake_case";
+let total_price = 49.99;
+let is_logged_in = false; //snake_case
+
+const MAX_SIZE = 100;
+const API_KEY = "abc123";
+const DATABASE_URL = "localhost"; //screaming snake case (constant variables)
+
+// 5. Hungarian Notation (prefix with type - older style)
+let strName = "string prefix";
+let bActive = true;       // boolean
+let nCount = 5;           // number
+let arrItems = [];        // array
+
+/*
+ *  This is multi line
+ *  Author : Prrmmod Dutta
+ *  Date : 14-Feb-2026
+ *  Date : 14-Feb-2026 // shift+alt+down arrows to duplicate line
+ */
+
+console.log(firstName + " " + lastName);
+function add(a,b){
+    return a+b;
+}
+console.log(add(10,20));
