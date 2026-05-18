34761ffb72b814eca1ce85cf82fbdb94e25add0c
monami2997 <immonami2997@gmail.com>
2026-05-18 09:10:06 +0530

    Incre-decre

--- js_basics/inc_decre.js
+++ js_basics/inc_decre.js
@@ -5,3 +5,15 @@
 let a = 5; 
 console.log(a++ + ++a - --a + a-- + ++a); 
 console.log(a);
+
+a=5;
+console.log(++a + a-- + --a +a++ +a);
+console.log(a);
+
+let x = 10;
+let y = x++ * --x + ++x - x-- + x;
+console.log(y);
+
+let m = 3;
+let n = m++ + ++m - m-- + --m + m;
+console.log(n);
