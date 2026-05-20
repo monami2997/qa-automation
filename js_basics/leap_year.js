961a39f29751465cbbcf1a9b9a6c39a972280d2e
monami2997 <immonami2997@gmail.com>
2026-05-20 07:48:52 +0530

    if_else

---
+++ js_basics/leap_year.js
@@ -0,0 +1,8 @@
+let year= 3000
+
+if (year%4===0 && year % 100 !==0)
+    console.log("Leap year");
+else if (year %400 ===0)
+    console.log("leap year");
+else
+    console.log("Not a leap year");
