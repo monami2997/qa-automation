9a525e8138f7df1d5f9ac277d30cd16376d2724b
monami2997 <immonami2997@gmail.com>
2026-05-23 11:33:51 +0530

    triangle

--- js_basics/triangles.js
+++ js_basics/triangles.js
@@ -1,8 +1,9 @@
-side1=56
+side1=8
 side2=54
-side3=58
-
-if(side1===side2 && side2===side3 && side1===side3)
+side3=6
+if (side1+side2 <= side3 || side1+side3 <= side2 || side3+side2 <= side1 )
+    console.log(`cannot form a tringle with ${side1},${side2} & ${side3}`);
+else if(side1===side2 && side2===side3)
     console.log("Equilateral");
 else if(side1===side2 || side2===side3)
     console.log("Isoceles");
