33d8d71b43d89b48d6ad7d0bc51fc87f49893844
monami2997 <immonami2997@gmail.com>
2026-06-03 12:09:54 +0530

    corrected

--- js_basics/strings/palindrome.js
+++ js_basics/strings/palindrome.js
@@ -1,13 +1,14 @@
 function palindrome(str){
-    let len=str.length;
+    str1=str.toLowerCase();
+    let len=str1.length;
     for(let i=0;i<len/2;i++)
         { 
-            if(str[i] !== str[len-1-i])
+            if(str1[i] !== str1[len-1-i])
                 return false;
                 
         }
     return true;
 }
 
-console.log(palindrome("madam"));
+console.log(palindrome("Madam"));
 console.log(palindrome("pramod"));
